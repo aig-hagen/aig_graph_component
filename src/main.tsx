75191cbc5313e94378ef0b5d9063fec54e84f9fe
Myriam Kasten <m.kasten@mailbox.org>
2023-05-24 17:11:32 +0200

    graph creation with form input

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import GraphComponent from "./components/graph/GraphComponent.tsx";
+import "../index.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <GraphComponent />
     </React.StrictMode>
 );
