7e2b760e1fb2eed817c434d79bcc8f24a2eb5f89
Myriam Kasten <m.kasten@mailbox.org>
2023-10-27 12:58:19 +0200

    comments on force layout

--- d3/simulation.ts
+++ d3/simulation.ts
@@ -16,16 +16,16 @@ export function createSimulation(
     return d3
         .forceSimulation<Node, Link>(graph!.nodes)
         .on('tick', () => onTick())
-        .force('charge', d3.forceManyBody<Node>().strength(-500))
-        .force('collision', d3.forceCollide<Node>().radius(config.nodeRadius))
+        .force('charge', d3.forceManyBody<Node>().strength(-500)) //how strong they repel/attract each other
+        .force('collision', d3.forceCollide<Node>().radius(config.nodeRadius)) //stop overlapping
         .force(
             'link',
             d3
-                .forceLink<Node, Link>()
+                .forceLink<Node, Link>() //for links to be a fixed distance apart
                 .links(graph!.links)
                 .id((d: Node) => d.id)
                 .distance(config.nodeRadius * 10)
         )
-        .force('x', d3.forceX<Node>(width / 2).strength(0.05))
+        .force('x', d3.forceX<Node>(width / 2).strength(0.05)) //attract elements towards specific positions
         .force('y', d3.forceY<Node>(height / 2).strength(0.05))
 }
