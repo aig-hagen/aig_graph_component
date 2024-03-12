83f0adb6b92d62540fe45598ff55ee69890a58cf
Myriam Kasten <m.kasten@mailbox.org>
2024-03-12 15:50:22 +0100

    formatting

--- src/main.ce.ts
+++ src/main.ce.ts
@@ -3,10 +3,10 @@ the vue3-webcomponent-wrapper (from the community) is used
 until official support (https://github.com/vuejs/vue-web-component-wrapper/issues/93).
 (We need it to use without shadow dom, since otherwise d3.js is not working properly.)*/
 
-import { createApp, h } from "vue";
+import { createApp, h } from 'vue'
 //@ts-ignore
-import wrapper from "vue3-webcomponent-wrapper";
-import GraphEditor from "@/components/GraphEditor.vue";
+import wrapper from 'vue3-webcomponent-wrapper'
+import GraphEditor from '@/components/GraphEditor.vue'
 
-const CustomElement = wrapper(GraphEditor, createApp, h);
-window.customElements.define("graph-editor", CustomElement);
+const CustomElement = wrapper(GraphEditor, createApp, h)
+window.customElements.define('graph-editor', CustomElement)
