26db129564f1e0aacfa4516b0d7b8d1a71ac20c3
Myriam Kasten <m.kasten@mailbox.org>
2024-03-12 15:58:36 +0100

    remove ce-compiler option since we don't use webcomponents inside vue components

--- vite.config.ts
+++ vite.config.ts
@@ -5,27 +5,19 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => tag.includes('graph-editor')
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
         }
-      }
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  build: {
-    lib: {
-      name: 'GraphComponentLib',
-      entry: './src/main.ce.ts',
     },
-  },
-  define: {
-    'process.env.NODE_ENV': "'production'"
-  }
+    build: {
+        lib: {
+            name: 'GraphComponentLib',
+            entry: './src/main.ce.ts'
+        }
+    },
+    define: {
+        'process.env.NODE_ENV': "'production'"
+    }
 })
