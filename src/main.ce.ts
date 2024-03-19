3d51011f34d85267c04efabb10d95b701dc9aa97
Myriam Kasten <m.kasten@mailbox.org>
2024-03-19 10:15:45 +0100

    vuetify plugin

--- src/main.ce.ts
+++ src/main.ce.ts
@@ -3,7 +3,7 @@ the apiCustomElement.ts (https://github.com/vuejs/core/issues/4314#issuecomment-
 from a community PR is used. (We need it to have a custom element without shadow dom,
 since otherwise d3.js is not working properly.) */
 
-import { defineCustomElement } from '@/external/apiCustomElement'
+import { defineCustomElement } from '@/plugins/apiCustomElement'
 import GraphEditor from '@/components/GraphEditor.vue'
 
 customElements.define('graph-editor', defineCustomElement(GraphEditor, { shadowRoot: false }))
