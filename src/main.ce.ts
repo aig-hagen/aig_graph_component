bd8731b4651e88b2f49bb355facda5c80fc40176
Myriam Kasten <myriam.kasten@fernuni-hagen.de>
2024-09-09 15:38:46 +0200

    build option without shadow root for mathjax in CE

--- src/main.ce.ts
+++ src/main.ce.ts
@@ -3,12 +3,14 @@ import { defineCustomElement } from 'vue'
 import { defineCustomElementWrapped } from '@/overrides/custom-element-with-plugin'
 import GraphEditor from '@/components/GraphEditor.vue'
 import vuetify from '@/plugins/vuetify'
+import { defineCustomElement as defineCustomElementWOShadowRoot } from '@/overrides/custom-element-remove-shadowroot'
 
 customElements.define(
     'graph-editor',
     // GUI Version
-    defineCustomElementWrapped(GraphEditor, { plugins: [vuetify] })
+    // defineCustomElementWrapped(GraphEditor, { plugins: [vuetify] })
     // CLI Version
     // defineCustomElement(GraphEditor)
+    // Without ShadowRoot for MathJax to work
+    defineCustomElementWOShadowRoot(GraphEditor, { shadowRoot: false })
 )
-//if for defineCustomElement you need to switch shadow root on and off, you can use the override located in overrides/custom-element-shadowroot.ts
