c98c05676719a58ad520ae473f8eed4cbcd4bff9
Myriam Kasten <myriam.kasten@fernuni-hagen.de>
2024-10-31 12:34:43 +0100

    dev server on all addresses

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,10 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
     },
+    server: {
+        host: '0.0.0.0',
+        port: 5173
+    },
     build: {
         lib: {
             name: 'GraphComponentLib',
