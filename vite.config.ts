f8281ebfb2c2356fef8765eb7b314354987b0afd
Myriam Kasten <myriam.kasten@fernuni-hagen.de>
2025-11-27 11:36:10 +0100

    #26 refactor vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,67 +1,79 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { ConfigEnv, defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
-const ceConfig = defineConfig({
-    plugins: [
-        vue({
-            // custom element mode -> inlines the style: https://github.com/vitejs/vite-plugin-vue/tree/main/packages/plugin-vue#using-vue-sfcs-as-custom-elements
-            // customElement: process.env.NODE_ENV === 'production'
-        })
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ command, mode }): UserConfig => {
+    if (command === 'build') {
+        if (mode === 'lib') {
+            return {
+                plugins: [
+                    vue(),
+                    dts({
+                        tsconfigPath: './tsconfig.app.json',
+                        outDir: './dist/lib/types'
+                    })
+                ],
+                resolve: {
+                    alias: {
+                        '@': fileURLToPath(new URL('./src', import.meta.url))
+                    }
+                },
+                build: {
+                    outDir: './dist/lib',
+                    lib: {
+                        name: 'GraphComponentLib',
+                        entry: './src/main.lib.js'
+                    },
+                    rollupOptions: {
+                        external: ['vue'],
+                        output: {
+                            globals: { vue: 'Vue' }
+                        }
+                    }
+                }
+            }
+        } else {
+            //custom element
+            return {
+                plugins: [
+                    vue({
+                        // custom element mode -> inlines the style: https://github.com/vitejs/vite-plugin-vue/tree/main/packages/plugin-vue#using-vue-sfcs-as-custom-elements
+                        // customElement: process.env.NODE_ENV === 'production'
+                    })
+                ],
+                resolve: {
+                    alias: {
+                        '@': fileURLToPath(new URL('./src', import.meta.url))
+                    }
+                },
+                build: {
+                    outDir: './dist/ce',
+                    lib: {
+                        name: 'GraphComponentCE',
+                        entry: './src/main.ce.ts'
+                    }
+                },
+                define: {
+                    'process.env.NODE_ENV': "'production'"
+                }
+            }
         }
-    },
-    server: {
-        host: '0.0.0.0',
-        port: 5173
-    },
-    build: {
-        outDir: './dist/ce',
-        lib: {
-            name: 'GraphComponentCE',
-            entry: './src/main.ce.ts'
-        }
-    },
-    define: {
-        'process.env.NODE_ENV': "'production'"
-    }
-})
-
-const libConfig = defineConfig({
-    plugins: [
-        vue(),
-        dts({
-            tsconfigPath: './tsconfig.app.json',
-            outDir: './dist/lib/types'
-        })
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    },
-    build: {
-        outDir: './dist/lib',
-        lib: {
-            name: 'GraphComponentLib',
-            entry: './src/main.lib.js'
-        },
-        rollupOptions: {
-            external: ['vue'],
-            output: {
-                globals: { vue: 'Vue' }
+    } else {
+        //dev
+        return {
+            plugins: [vue()],
+            resolve: {
+                alias: {
+                    '@': fileURLToPath(new URL('./src', import.meta.url))
+                }
+            },
+            server: {
+                host: '0.0.0.0',
+                port: 5173
             }
         }
     }
 })
-
-export default ({ mode }: ConfigEnv) => {
-    if (mode === 'lib') return libConfig
-    else return ceConfig
-}
