b68d9a015947235b08bd5329ce74a45734fb1c3a
Myriam Kasten <myriam.kasten@fernuni-hagen.de>
2025-12-15 11:02:35 +0100

    #22 using vuetify

---
+++ application-examples/vue-component/plugins/vuetify.ts
@@ -0,0 +1,30 @@
+import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
+import {
+    mdiCogOutline,
+    mdiDelete,
+    mdiFileImportOutline,
+    mdiHelp,
+    mdiHelpCircleOutline,
+    mdiImageFilterCenterFocus,
+    mdiPlusThick
+} from '@mdi/js'
+
+export default createVuetify({
+    icons: {
+        defaultSet: 'mdi',
+        aliases: {
+            ...aliases,
+            addNode: mdiPlusThick,
+            deleteGraph: mdiDelete,
+            help: mdiHelp,
+            importExport: mdiFileImportOutline,
+            resetView: mdiImageFilterCenterFocus,
+            settings: mdiCogOutline,
+            helpCircle: mdiHelpCircleOutline
+        },
+        sets: {
+            mdi
+        }
+    }
+})
--- application-examples/vue-component/src/main.ts
+++ application-examples/vue-component/src/main.ts
@@ -4,5 +4,6 @@ import App from './App.vue'
 // Graph Component
 import { GraphComponent } from '../dependencies/graph-component/graph-component.js'
 import '../dependencies/graph-component/graph-component.css'
+import vuetify from '../plugins/vuetify.ts'
 
-createApp(App).component('GraphComponent', GraphComponent).mount('#app')
+createApp(App).component('GraphComponent', GraphComponent).use(vuetify).mount('#app')
