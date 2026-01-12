6dab46f23d0dc9305f8fb9659fedf00cc851cd8a
Myriam Kasten <myriam.kasten@fernuni-hagen.de>
2026-01-12 14:11:48 +0100

    preparation for npm package
    Co-authored-by: Oleksandr Dzhychko <oleksandr.dzhychko@fernuni-hagen.de>

--- src/main.lib.ts
+++ src/main.lib.ts
@@ -1,3 +1,7 @@
 import GraphComponent from '@/components/GraphComponent.vue'
 
+export type { GraphConfigurationPublic } from '@/components/GraphComponent.vue'
+export type * from '@/model/config.ts'
+export type * from '@/model/graph-node.ts'
+export type * from '@/model/graph-link.ts'
 export { GraphComponent }
