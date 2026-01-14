fd5a8d2cda6a2f7e4e8e9a5f4925e1734145b1f4
Myriam Kasten <myriam.kasten@fernuni-hagen.de>
2026-01-14 13:59:56 +0100

    export type parser

--- src/main.lib.ts
+++ src/main.lib.ts
@@ -1,7 +1,8 @@
 import GraphComponent from '@/components/GraphComponent.vue'
 
-export type { GraphConfigurationPublic } from '@/components/GraphComponent.vue'
+export type * from '@/components/GraphComponent.vue'
 export type * from '@/model/config.ts'
 export type * from '@/model/graph-node.ts'
 export type * from '@/model/graph-link.ts'
+export type * from '@/model/parser.ts'
 export { GraphComponent }
