63db4cf1c8b0088b2644c880fec88a009282ec6b
Oleksandr Dzhychko <hey@oleks.dev>
2026-01-14 13:57:53 +0100

    remove duplicate import of style sheet

--- application-examples/vue-component/src/main.ts
+++ application-examples/vue-component/src/main.ts
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// Graph Component
-import '@aig-hagen/graph-component/dist/lib/graph-component.css'
 import vuetify from '../plugins/vuetify.ts'
 
 createApp(App).use(vuetify).mount('#app')
