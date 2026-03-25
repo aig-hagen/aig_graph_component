e852e0b7e7abc6d278e6b3c69bbac7df5f473433
Oleksandr Dzhychko <hey@oleks.dev>
2026-03-25 09:23:04 +0100

    test: introduce test fixtures

--- src/components/GraphComponent.ct.spec.tsx
+++ src/components/GraphComponent.ct.spec.tsx
@@ -1,79 +1,112 @@
-import { test, expect, type MountResultJsx } from '@playwright/experimental-ct-vue'
+import { test as base, expect, type MountResultJsx } from '@playwright/experimental-ct-vue'
 import GraphComponent from '@/components/GraphComponent.vue'
+import type { Page } from 'playwright/test'
 
-test('show controls if empty', async ({ mount, page }) => {
-    const component = await mount(<GraphComponent />)
+// Use fixtures for cleaner tests.
+// See https://playwright.dev/docs/test-fixtures
+const test = base.extend<{ component: MountResultJsx; graph: GraphFixture }>({
+    component: async ({ mount }, use) => {
+        const component = await mount(<GraphComponent />)
+        await use(component)
+    },
+    graph: async ({ component, page }, use) => {
+        const graph = new GraphFixture(component, page)
+        await use(graph)
+    }
+})
 
+test('show controls if empty', async ({ component, page }) => {
     await expect(component.getByText('Controls')).toBeVisible()
     await expect(page).toHaveScreenshot()
 })
 
-test('create loop', async ({ mount, page }) => {
-    const position = { x: 150, y: 150 }
-    const component = await mount(<GraphComponent />)
-    await createNode(component, position)
+test('create loop', async ({ graph, page }) => {
+    const node = await graph.createNode({ x: 150, y: 150 })
 
-    await component.click({ position, button: 'right' })
+    await node.createSelfLoop()
 
     await expect(page).toHaveScreenshot()
 })
 
-test('update label', async ({ mount, page }) => {
-    const position = { x: 150, y: 150 }
-    const component = await mount(<GraphComponent />)
-    await createNode(component, position)
+test('update label', async ({ graph, page }) => {
+    const node = await graph.createNode({ x: 150, y: 150 })
 
-    await component.click({ position })
-    // After click, input should be focused and we can type.
-    await page.keyboard.type('a label')
-    await page.keyboard.press('Enter')
+    await node.enterLabel('a label')
 
     await expect(page).toHaveScreenshot()
 })
 
-test('expose getNodeSize', async ({ mount }) => {
-    const component = await mount(<GraphComponent />)
-    await createNode(component, { x: 150, y: 150 })
+test('expose getNodeSize', async ({ graph }) => {
+    await graph.createNode({ x: 150, y: 150 })
 
-    const nodeSize = await evaluateOnComponent(component, (instance) => instance.getNodeSize(0))
+    const nodeSize = await graph.evaluateOnComponent((instance) => instance.getNodeSize(0))
 
     expect(nodeSize).toEqual({ height: 48, width: 128 })
 })
 
-test('expose centerView', async ({ mount, page }) => {
-    const component = await mount(<GraphComponent />)
-    await createNode(component, { x: 150, y: 150 })
-    await createNode(component, { x: 300, y: 300 })
-    await evaluateOnComponent(component, (instance) => instance.toggleZoom(true))
+test('expose centerView', async ({ graph, page }) => {
+    await graph.createNode({ x: 150, y: 150 })
+    await graph.createNode({ x: 300, y: 300 })
+    await graph.evaluateOnComponent((instance) => instance.toggleZoom(true))
     // Auto growing labels currently break size of label when zooming.
     // https://github.com/aig-hagen/aig_graph_component/issues/29
-    await evaluateOnComponent(component, (instance) => instance.toggleNodeAutoGrow(false))
+    await graph.evaluateOnComponent((instance) => instance.toggleNodeAutoGrow(false))
 
-    await evaluateOnComponent(component, (instance) =>
+    await graph.evaluateOnComponent((instance) =>
         instance.centerView({ top: 5, right: 25, bottom: 50, left: 100 })
     )
 
     await expect(page).toHaveScreenshot()
 })
 
-async function createNode(component: MountResultJsx, position: { x: number; y: number }) {
-    return await component.dblclick({ position })
+interface Position {
+    x: number
+    y: number
 }
 
-// XXX: This is a discouraged pattern.
-// Components should communicate only via props and emits.
-// However, the graph component relies heavily on methods exposed via `defineExpose()`,
-// because it can also be used as a custom element.
-// This pattern is therefore kept in place to test those exposed methods.
-async function evaluateOnComponent<ReturnT>(
-    component: MountResultJsx,
-    fn: (instance: InstanceType<typeof GraphComponent>) => ReturnT
-) {
-    return await component.evaluate((rootEl, fnSerialized) => {
-        const instance = (rootEl as any)._vnode.component.subTree.component.exposed as InstanceType<
-            typeof GraphComponent
-        >
-        fn = eval(fnSerialized)
-        return fn(instance)
-    }, fn.toString())
+class GraphFixture {
+    constructor(
+        private component: MountResultJsx,
+        private page: Page
+    ) {}
+
+    async createNode(position: Position) {
+        await this.page.mouse.dblclick(position.x, position.y)
+        return new NodeFixture(this.page, position)
+    }
+
+    // XXX: This is a discouraged pattern.
+    // Components should communicate only via props and emits.
+    // However, the graph component relies heavily on methods exposed via `defineExpose()`,
+    // because it can also be used as a custom element.
+    // This pattern is therefore kept in place to test those exposed methods.
+    async evaluateOnComponent<ReturnT>(
+        fn: (instance: InstanceType<typeof GraphComponent>) => ReturnT
+    ) {
+        const fnSerialized = fn.toString()
+        return await this.component.evaluate((rootEl, fnSerialized) => {
+            const instance = (rootEl as any)._vnode.component.subTree.component
+                .exposed as InstanceType<typeof GraphComponent>
+            fn = eval(fnSerialized)
+            return fn(instance)
+        }, fnSerialized)
+    }
+}
+
+class NodeFixture {
+    constructor(
+        private page: Page,
+        private position: Position
+    ) {}
+
+    async createSelfLoop() {
+        await this.page.mouse.click(this.position.x, this.position.y, { button: 'right' })
+    }
+
+    async enterLabel(text: string) {
+        await this.page.mouse.click(this.position.x, this.position.y)
+        // After click, input should be focused and we can type.
+        await this.page.keyboard.type(text)
+        await this.page.keyboard.press('Enter')
+    }
 }
